a5a5d022fe44aa1e1540cba7b923ee05702e57e7
Kunal Sharma <ksharma8@terpmail.umd.edu>
2014-10-31 16:32:08 -0400

    updates

--- lab5/product_dedup.py
+++ lab5/product_dedup.py
@@ -122,13 +122,7 @@ def title_comparator(field_1, field_2) :
             if word in field_1:
                 f2_f1 += 1
 
-        f1_f2 /= len(field_2)
-        f2_f1 /= len(field_1)
-
-        f1_f2 *= 100
-        f2_f1 *= 100
-
-        return (f1_f2 + f2_f1)/2
+        return (1000 * f1_f2 * f2_f1) / (len(field_1) * len(field_2))
 
     else :
         return nan
