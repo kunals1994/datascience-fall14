47506d4732bdbb16deb7c39ff101e7f34b9fe2d8
Kunal Sharma <ksharma8@terpmail.umd.edu>
2014-11-09 19:15:38 -0500

    updates

--- lab6/BigramCount.java
+++ lab6/BigramCount.java
@@ -54,8 +54,8 @@ package org.myorg;
                 String one = line.substring(0, spaceOne);
                 String two = line.substring(spaceOne + 1, spaceTwo);
 
-                output.collect(new Text(one), value);
-                output.collect(new Text(two), value);
+                output.collect(new Text(one), new Text(line));
+                output.collect(new Text(two), new Text(line));
             }
         }
 	
